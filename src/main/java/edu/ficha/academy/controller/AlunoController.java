b7af39ecdcecd90d942cba1c39fc0c9d4c7599d9
herickaquino1 <rherickaquino18@gmail.com>
2024-03-02 17:08:31 -0300

    Uso da anotação @Modifiyng e @Transactional na contrução dos metodos de atualizar objetivo e nome

--- src/main/java/edu/ficha/academy/controller/AlunoController.java
+++ src/main/java/edu/ficha/academy/controller/AlunoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,16 @@ public class AlunoController {
 		al = aluno.findByMatricula(matricula);
 		return "Nome: " + al.getNome() +  " | " + "Objetivo: " + al.getObjetivo() + " | " +
 				"Peso: " + al.getPeso() + " | " + "Altura: " + al.getAltura();
+	}
+	
+	@PutMapping("/atualizar/objetivo/{objetivo}/{nome}")
+	public void atualizarObjetivo(@PathVariable("objetivo") String objetivo,@PathVariable("nome") String nome) {
+		aluno.atualizarObjetivo(objetivo, nome);
+	}
+	
+	@PutMapping("/atualizar/nome/{novoNome}/{nome}")
+	public void atualizarNome(@PathVariable("novoNome") String novoNome,@PathVariable("nome") String nome) {
+		aluno.atualizarNome(novoNome, nome);
 	}
 	
 	
--- src/main/java/edu/ficha/academy/repository/AlunoRepository.java
+++ src/main/java/edu/ficha/academy/repository/AlunoRepository.java
@@ -3,9 +3,13 @@ package edu.ficha.academy.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import edu.ficha.academy.model.AlunoModel;
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<AlunoModel, Integer> {
@@ -14,6 +18,15 @@ public interface AlunoRepository extends JpaRepository<AlunoModel, Integer> {
 	
 	public AlunoModel findByMatricula(int id);
 	
+	@Transactional
+	@Modifying
+	@Query("update AlunoModel a set a.objetivo = :objetivo where a.nome =:nome ")
+	public void atualizarObjetivo(@Param("objetivo") String objetivo, @Param("nome") String nome);
+	
+	@Transactional
+	@Modifying
+	@Query("update AlunoModel a set a.nome =:novoNome where a.nome =:nome")
+	public void atualizarNome(@Param("novoNome") String novoNome, @Param("nome") String nome);
 
 
 }
